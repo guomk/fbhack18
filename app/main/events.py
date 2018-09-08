c5298231c8a6ce7a6dac26ac48406062468a5e7d
yangyang <yangyang@yangyangdeMacBook-Pro.local>
2018-09-08 22:38:00 +0800

    newest version

--- app/main/events.py
+++ app/main/events.py
@@ -1,23 +1,75 @@
-from flask import session
+from flask import session, request
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 
+usernames = {}
+number_of_users = 0
+# When the client emits 'connection', this listens and executes
+@socketio.on('connection', namespace='/chat')
+def user_connected():
+	room = session.get('room')
+	print('User connected')
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
     room = session.get('room')
+    print(request.sid)
     join_room(room)
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
 
+# When client emits 'add user' this listens and executes
+@socketio.on('add user', namespace='/chat')
+def add_user(data):
+	print ('Adding User')
+	global usernames
+	global number_of_users
 
-@socketio.on('text', namespace='/chat')
-def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
-    room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+	session['username'] = data
+	usernames[data] = session['username']
+
+	number_of_users += 1;
+
+	emit('login', { 'numUsers' : number_of_users })
+	emit('user joined', { 'username' : session['username'], 'numUsers': number_of_users }, broadcast=True)
+
+@socketio.on('typing', namespace='/chat')
+def typing_response():
+	room = session.get('room')
+	try:
+		emit('typing', { 'username' : session['username'] }, room=room)
+	except:
+		pass
+
+@socketio.on('stop typing', namespace='/chat')
+def stop_typing():
+	room = session.get('room')
+	try:
+		emit('stop typing', { 'username' : session['username'] }, room=room)
+	except:
+		pass
+
+@socketio.on('disconnect', namespace='/chat')
+def disconnect():
+	global usernames
+	global number_of_users
+	room = session.get('room')
+
+	try:
+		del usernames[session['username']]
+		number_of_users -= 1
+		emit('user left', { 'username' : session['username'], 'numUsers' : number_of_users}, room=room)
+
+	except:
+		pass
+
+@socketio.on('new message', namespace='/chat')
+def new_message(data):
+	room = session.get('room')
+	emit('new message',
+		{ 'username' : session['username'],
+	 	'message': data }, room=room)
 
 
 @socketio.on('left', namespace='/chat')
